a95410be330341f626333cc9b689476f9b5bee3b
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-06-18 21:57:29 -0300

    Atualizando inicialização de referência de banco de dados

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
-from sql_alchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", 
+                        "postgresql://postgres:senha123@localhost5432/pathufes")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
