4a37fc8312a0bd853ba6bc6ef9fcc1ca25956a42
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-07-01 14:25:21 -0300

    Inserindo/adaptando/corrigindo função de upload de arquivo PDF

--- frontend/src/services/grafoService.js
+++ frontend/src/services/grafoService.js
@@ -3,3 +3,7 @@ import {api} from "./api"
 export function fetchGrafo() {
     return api.get("/grafo");
 }
+
+export function uploadPdf(file) {
+    return api.postFile("aluno/upload-pdf", file);
+}
