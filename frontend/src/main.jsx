b15005a237dffe6c6c93f63c777296a8aea9b010
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-07-02 18:29:14 -0300

    Inserindo comentários de código em arquivo principal

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,15 @@
+/**
+ * Importa componentes de renderização ("createRoot"), ativa verificações e avisos (durante o desenvolvimento da aplicação - "StrictMode"),
+ * importa estilos e a aplicação do componente raíz do projeto;
+ */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+/**
+ * Função que cria conexão com a raíz da aplicação (em HTML) e renderiza o aplicativo; 
+ */
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
