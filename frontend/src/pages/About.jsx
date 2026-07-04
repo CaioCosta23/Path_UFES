e421b9270ae7809dde897e443bd740d155031e38
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-07-04 14:36:13 -0300

    Inserindo comentários de documentação da página 'About'

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,20 @@
+/**
+ * Importa a biblioteca de estilização da página "About";
+ */
 import styles from "../styles/About.module.css";
 
+/**
+ * Importa as imagens usadas para os avatares dos desenvolvedores na página;
+ */
 import fotoDev1 from "../assets/images/authors/Caio-Path_UFES.jpg"
+import fotoDev2 from "../assets/images/authors/Daniel-Path_UFES.jpg"
+import fotoDev3 from "../assets/images/authors/Miguel-Path_UFES.jpg"
 
+/**
+ * Declaração do componente que será exportado de forma padrão (para a renderização da página);
+ * 
+ * @returns {import("react").ReactElement} Elemento React representando a página "About";
+ */
 export default function About() {
     return (
         <div className = {styles.container}>
@@ -16,7 +29,10 @@ export default function About() {
                 </p>
             </section>
 
-            {/* Desenvolvedores */}
+            {/*
+             * Desenvolvedores:
+                Seção que contém o "Container" que organiza os cards onde serão colocadas as informações dos desenvolvedores;
+             */}
             <section className = {styles.section}>
                 <h2 className = {styles.sectionTitle}>Desenvolvedores</h2>
                 <div className = {styles.devGrid}>
@@ -25,11 +41,11 @@ export default function About() {
                         <h3 className = {styles.devName}>Caio Costa Lopes</h3>
                     </div>
                     <div className = {styles.devCard}>
-                        <div className = {styles.devAvatar}>avatar</div>
+                        <img src = {fotoDev2} alt = "Daniel Sbrocco Olimpio" className = {styles.devAvatar}/>
                         <h3 className = {styles.devName}>Daniel Sbrocco Olimpio</h3>
                     </div>
                     <div className = {styles.devCard}>
-                        <div className = {styles.devAvatar}>avatar</div>
+                        <img src = {fotoDev3} alt = "Miguel Zon Murad" className = {styles.devAvatar}/>
                         <h3 className = {styles.devName}>Miguel Zon Murad</h3>
                     </div>
                     
