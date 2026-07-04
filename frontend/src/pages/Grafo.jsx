193637dffde1f8cb4dfa2c0230331ec12b035082
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-07-04 14:54:45 -0300

    Inserindo comentários de documentação da página Grafo

--- frontend/src/pages/Grafo.jsx
+++ frontend/src/pages/Grafo.jsx
@@ -1,8 +1,18 @@
+/**
+ * Importa o módulo do visualizador do grafo e de estilização da página;
+ */
 import GrafoViewer from "../components/GrafoViewer";
 import styles from "../styles/Grafo.module.css";
 
+/**
+ * 
+ * @returns {import("react").ReactElement} Elemento React que representa a página "Grafo";
+ */
 export default function Grafo() {
     return (
+        /**
+         * "Container que contem descrição e cabeçalho de página;"
+         */
         <div className = {styles.container}>
             <div className = {styles.header}>
                 <h1 className = {styles.title}>Visualizador de Grafo</h1>
@@ -10,6 +20,9 @@ export default function Grafo() {
                     Carregue um PDF ou arquivo ".json" para visualizar a grade curricular.
                 </p>
             </div>
+            {/**
+             * Renderiza o visualizador do grafo (utilizando as funcionalidades e propriedades utilizadas no "GrafoViewer.jsx");
+             */}
             <GrafoViewer/>
         </div>
     );
