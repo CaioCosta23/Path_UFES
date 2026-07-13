334b4375f1da14b14bce277cbe6cece243a9f1b9
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-07-13 15:27:31 -0300

    Inserindo comentários e corrigindo erros de identação e lógica

--- frontend/src/tests/pages/Home.test.jsx
+++ frontend/src/tests/pages/Home.test.jsx
@@ -1,8 +1,14 @@
+/**
+ * Importa funções de teste, renderização e roteamento de página;
+ */
 import {describe, it, expect} from "vitest";
 import {render, screen} from "@testing-library/react";
 import {MemoryRouter} from "react-router-dom";
 import Home from "../../pages/Home";
 
+/**
+ * Configuração de descrição de teste;
+ */
 describe("Home", () => {
     const renderHome = () =>
         render(
@@ -11,13 +17,15 @@ describe("Home", () => {
             </MemoryRouter>
         );
     
+    // Busca o elemento que representa o título na página e o que ele espera visualizar;
     it("deve renderizar o título principal", () => {
         renderHome();
 
         expect(
             screen.getByText("Visualizador de Grade Curricular")).toBeInDocument();
     });
-
+    
+    // Busca o elemento que representa a descrição da página e o que ele espera visualizar;
     it ("deve renderizar a descrição do projeto", () => {
         renderHome();
 
@@ -25,28 +33,31 @@ describe("Home", () => {
             screen.getByText("/Explore as matérias e seus pré-requisitos/i")).toBeInDocument();
     });
 
+    // Busca o elemento que representa a o botão do grafo e o que ele deve visualizar;
     it ("deve renderizar o botão para o grafo", () => {
         renderHome();
 
         const botao = screen.getByText("Visuializar Grafo ->");
         expect(botao).toBeInDocument();
         expect(botao.closest("a")).toHaveAttribute("href", "/grafo");
+    });
 
-        it("deve renderizar os três cards de features", () => {
-            renderHome();
+    // Busca o elemento que representa os "cards" das "features" o que espera visualizar;
+    it("deve renderizar os três cards de features", () => {
+        renderHome();
 
-            expect(screen.getByText("Upload de PDF")).toBeInDocument();
-            expect(screen.getByText("Pré-Requisitos")).toBeInDocument();
-            expect(screen.getByText("Interativo")).toBeInDocument();
-        });
+        expect(screen.getByText("Upload de PDF")).toBeInDocument();
+        expect(screen.getByText("Pré-Requisitos")).toBeInDocument();
+        expect(screen.getByText("Interativo")).toBeInDocument();
+    });
 
-        it ("deve renderizar os ícones dos cards", () => {
-            renderHome();
+    // Busca os elementos que representam os ícones dos "cards" das páginas e o que espera visualizar;
+    it ("deve renderizar os ícones dos cards", () => {
+        renderHome();
 
-            // Sujeito a alterações por causa dos emojis;
-            expect(screen.getByText("paper")).toBeInDocument();
-            expect(screen.getByText("link")).toBeInDocument();
-            expect(screen.getByText("search")).toBeInDocument();
-        })
+        // Sujeito a alterações por causa dos emojis;
+        expect(screen.getByText("paper")).toBeInDocument();
+        expect(screen.getByText("link")).toBeInDocument();
+        expect(screen.getByText("search")).toBeInDocument();
     });
 });
