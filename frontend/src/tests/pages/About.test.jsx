b9f026964015641bbbf959870e80f744166dcf99
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-07-13 22:30:23 -0300

    Inserindo comentários de documentação

--- frontend/src/tests/pages/About.test.jsx
+++ frontend/src/tests/pages/About.test.jsx
@@ -1,8 +1,14 @@
+/**
+ * Importa as funções de teste, renderização e Roteamento/direcionamento de página;
+ */
 import {describe, it, expect} from "vitest";
 import {render, screen} from "@testing-library/react";
 import {MemoryRouter} from "react-router-dom";
 import About from "../../pages/About";
 
+/**
+ * Função auxiliar de configuração para o teste;
+ */
 describe("About", () => {
     const renderAbout = () =>
         render(
@@ -10,19 +16,22 @@ describe("About", () => {
                 <About/>
             </MemoryRouter>
         );
-
+    
+    // Busca o elemento que representa o título da página e o que espera visualizar;
     it("deve renderizar o título principal", () => {
         renderAbout();
 
         expect(screen.getByText(/sobre o projeto/i)).toBeInTheDocument();
     });
 
+    // Busca o elemento que representa a descrição da página e o que espera visualizar;
     it("deve renderizar a descrição do projeto", () => {
         renderAbout();
 
         expect(screen.getByText(/ferramenta de planejamento acadêmico/i)).toBeInTheDocument();
     });
 
+    // Busca o elemento que representa o link de repositório do projeto e o que espera visualizar;
     it("deve renderizar o link do GitHub do projeto", () => {
         renderAbout();
 
@@ -34,6 +43,7 @@ describe("About", () => {
         );
     });
 
+    // Busca o elemento que representa o link para o repositório GitHub do projeto e o que espera visualizar;
     it("deve abrir o link do GitHub em nova aba", () => {
         renderAbout();
 
@@ -41,6 +51,7 @@ describe("About", () => {
         expect(link).toHaveAttribute("target", "_blank");
     });
 
+    // Busca o elemento que representa a seção de apresnetação dos desenvolvedores e o que espera visualizar;
     it("deve renderizar a seção de desenvolvedores", () => {
         renderAbout();
 
@@ -50,9 +61,12 @@ describe("About", () => {
         expect(screen.getByText("Miguel Zon Murad")).toBeInTheDocument();
     });
 
+    // Busca o elemento que representa os "cards" (cartão/foto de apresentação) dos desenvolvedores e o que espera visualizar;
     it("deve renderizar três desenvolvedores", () => {
+        // Container de redenrização da página;
         const {container} = renderAbout();
 
+        // Área de renderização dos cartões de apresentação dos desenvolvedores;
         const devCards = container.querySelectorAll("[class*='devCard']");
         expect(devCards.length).toBe(3);
     });
