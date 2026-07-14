34843eeaf9c09dc8abdc0c2216c98dbbccc54acb
CaioCosta23 <caio.c.lopes@edu.ufes.br>
2026-07-13 22:11:10 -0300

    Inserindo comentários de documentação

--- frontend/src/tests/pages/Grafo.test.jsx
+++ frontend/src/tests/pages/Grafo.test.jsx
@@ -1,12 +1,22 @@
+/**
+ * Importa as funções de teste, renderização e Roteamento/direcionamento de página;
+ */
 import {describe, it, expect, vi} from "vitest";
 import {render, screen} from "@testing-library/react";
 import {MemoryRouter} from "react-router-dom";
 import Grafo from "../../pages/Grafo";
 
+/**
+ * Módulo que subistitui função pelo objeto real que seria inserido/utilizado na aplicação para fins de teste,
+ * e verificando se o mesmo renderiza;
+ */
 vi.mock("../../components/GrafoViewer", () => ({
     default: () => <div data-testid = "grafo-viewer"/>
 }));
 
+/**
+ * Função auxiliar de configuração para o teste;
+ */
 describe("Grafo", () => {
     const renderGrafo = () =>
         render(
@@ -15,6 +25,7 @@ describe("Grafo", () => {
             </MemoryRouter>
         );
 
+    // Busca o elemento que representa o título da página e o que espera visualizar;
     it("deve renderizar o título da página", () => {
         renderGrafo();
 
@@ -22,6 +33,7 @@ describe("Grafo", () => {
             screen.getByText("Visualizador de Grafo")).toBeInTheDocument();
     });
 
+    // Busca o elemento que representa o subtítulo da página e o que espera visualizar;
     it("deve renderizar o subtítulo", () => {
         renderGrafo();
 
@@ -29,6 +41,7 @@ describe("Grafo", () => {
             screen.getByText(/Carregue um PDF/i)).toBeInTheDocument();
     });
 
+    // Busca o elemento que representa visualizador gráfico do Grafo na página e o que espera visualizar;
     it("deve renderizar o GrafoViewer", () => {
         renderGrafo();
 
